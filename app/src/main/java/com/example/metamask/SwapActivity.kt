32c60d05e34fddf055792f7759e28371c0241287
WrappingorNot <WrappingorNot@github.com>
2023-03-29 15:08:51 +0900

    Swap 작업중_ 네트워크 추가 Retrofit 화면 연결 확인

--- app/src/main/java/com/example/metamask/SwapActivity.kt
+++ app/src/main/java/com/example/metamask/SwapActivity.kt
@@ -1,9 +1,15 @@
 package com.example.metamask
 
+import android.os.Build
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.SearchView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.example.metamask.Retrofit.accountResponse
 import com.example.metamask.Retrofit.initMyApi
 import com.example.metamask.Retrofit.priceResponse
@@ -14,30 +20,25 @@ import retrofit2.Call
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.Body
 
 class SwapActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySwapBinding
+    private lateinit var adapter: SwapBottomdialogAdapter
+    private lateinit var bottomdialog: SwapBottomdialog
+    private var list_market = mutableListOf<BottomDialogItem>()
+    private val filteredList = mutableListOf<BottomDialogItem>()
+    private var exchangerate: Int = 0
+
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySwapBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        list_market = ArrayList<BottomDialogItem>()
 
-        getMarket()
-        getPrice()
-
-
-        binding.coinSelect1.setOnClickListener(View.OnClickListener {
-            val bottomdialog = SwapBottomdialog()
-
-            bottomdialog.show(supportFragmentManager, bottomdialog.tag)
-        })
-
-    }
+        val adapter = SwapBottomdialogAdapter()
 
 
-    fun getMarket(){
-        //로그를 보기위한 Intercepter
         val interceptor1 = HttpLoggingInterceptor()
         interceptor1.setLevel(HttpLoggingInterceptor.Level.BODY)
         val client = OkHttpClient.Builder()
@@ -50,6 +51,12 @@ class SwapActivity : AppCompatActivity() {
             .client(client) //로그 기능
             .build()
         var initMyApi = retrofit.create(initMyApi::class.java)
+
+
+
+
+
+        //종목들 조회후 가져와서  recyclerView에 담기
 
         initMyApi.getMarketResponse()?.enqueue(object :
             retrofit2.Callback<MutableList<accountResponse?>?> {
@@ -60,13 +67,95 @@ class SwapActivity : AppCompatActivity() {
                 var result : MutableList<accountResponse?>? = response.body()
 
                 Log.d("123", result?.get(0)?.korean_name.toString())
+                for (i in 0 until result?.size!!){
+                    list_market.add(BottomDialogItem (result?.get(i)?.market.toString()))
+                    filteredList.add(BottomDialogItem (result?.get(i)?.market.toString()))
+                }
+                adapter.setItem(list_market)
+
             }
 
             override fun onFailure(call: Call<MutableList<accountResponse?>?>, t: Throwable) {
             }
         })
+
+        //가격 가져오기
+        val market: String = "KRW-ETH"
+        initMyApi.getPriceResponse(markets = market)?.enqueue(object :
+            retrofit2.Callback<MutableList<priceResponse?>?> {
+            override fun onResponse(
+                call: Call<MutableList<priceResponse?>?>,
+                response: Response<MutableList<priceResponse?>?>
+            ) {
+                var result : MutableList<priceResponse?>? = response.body()
+                exchangerate = result?.get(0)?.trade_price?.toInt() ?:
+                        Log.d("123", result?.get(0)?.trade_price.toString())
+                binding.tvExchange.text = "변환 후 :"+ 0 + "원"
+
+            }
+
+            override fun onFailure(call: Call<MutableList<priceResponse?>?>, t: Throwable) {
+            }
+        })
+        binding.btnExchange.setOnClickListener(View.OnClickListener {
+            binding.tvExchange.text = "변환 후 :"+ exchangerate*(Integer.parseInt(binding.moneyExchange.text.toString())) + "원"
+        })
+
+        binding.btnUpdown.setOnClickListener(View.OnClickListener {
+
+        })
+
+
+
+        val bottomdialog = SwapBottomdialog(adapter)
+
+        binding.coinSelect1.setOnClickListener(View.OnClickListener {
+
+
+
+            bottomdialog.show(supportFragmentManager, bottomdialog.tag)
+        })
+        val searchview = bottomdialog.view?.findViewById<SearchView>(R.id.search_view)
+
+        searchview?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+
+                filteredList.clear()
+                ( 0 until list_market.size)?.filter{
+                    list_market[it].toString().toLowerCase().contains(newText.toLowerCase())
+                }?.mapTo(filteredList) { list_market[it] }
+
+                var adapter1 = SwapBottomdialogAdapter()
+                adapter1.setItem(filteredList)
+                return false
+            }
+        })
+
+        binding.coinSelect2.setOnClickListener(View.OnClickListener {
+
+
+            bottomdialog.show(supportFragmentManager, bottomdialog.tag)
+
+        })
+
+        getPrice()
+
     }
-    fun getPrice(){
+
+
+
+
+    fun getMarket( list_market: ArrayList<BottomDialogItem>){
+        //로그를 보기위한 Intercepter
+
+    }
+    fun getPrice() {
         val interceptor1 = HttpLoggingInterceptor()
         interceptor1.setLevel(HttpLoggingInterceptor.Level.BODY)
         val client = OkHttpClient.Builder()
@@ -79,21 +168,6 @@ class SwapActivity : AppCompatActivity() {
             .client(client) //로그 기능
             .build()
         var initMyApi = retrofit.create(initMyApi::class.java)
-        val market: String = "KRW-ETH"
-        initMyApi.getPriceResponse(markets = market)?.enqueue(object :
-            retrofit2.Callback<MutableList<priceResponse?>?> {
-            override fun onResponse(
-                call: Call<MutableList<priceResponse?>?>,
-                response: Response<MutableList<priceResponse?>?>
-            ) {
-                var result : MutableList<priceResponse?>? = response.body()
 
-                Log.d("123", result?.get(0)?.trade_price.toString())
-
-            }
-
-            override fun onFailure(call: Call<MutableList<priceResponse?>?>, t: Throwable) {
-            }
-        })
     }
 }
--- app/src/main/java/com/example/metamask/SwapBottomdialog.kt
+++ app/src/main/java/com/example/metamask/SwapBottomdialog.kt
@@ -1,18 +1,20 @@
 package com.example.metamask
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.metamask.databinding.BottomDialogBinding
 import com.example.metamask.databinding.BottomSwapBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class SwapBottomdialog(): BottomSheetDialogFragment() {
+class SwapBottomdialog(var adapter: SwapBottomdialogAdapter): BottomSheetDialogFragment() {
     private lateinit var binding: BottomSwapBinding
 
-    private lateinit var listAdapter: ArrayAdapter<String>
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,9 +26,48 @@ class SwapBottomdialog(): BottomSheetDialogFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        binding.recyclerView1.adapter = adapter
+        Log.d("1234", "attached")
+
 
 
 
     }
+
+
+}
+
+
+data class BottomDialogItem(var name:String)
+class SwapBottomdialogAdapter : RecyclerView.Adapter<SwapBottomdialogAdapter.Holder>() {
+    private var itemList: MutableList<BottomDialogItem> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.network_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        val item = itemList[position]
+        holder.bind(item)
+    }
+
+    fun setItem(items: MutableList<BottomDialogItem>) {
+        if (!items.isNullOrEmpty()) {
+            itemList = items
+            notifyDataSetChanged()
+        }
+    }
+
+    inner class Holder(val view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: BottomDialogItem) {
+            view.findViewById<TextView>(R.id.tv_netName).text = item.name
+        }
+    }
+
 
 }
